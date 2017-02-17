2dd00d5a55cd0864391c3f897706b26fe1cda403
Richard Held <Richard@Richards-iMac.local>
2017-02-17 18:44:38 -0500

    Begin display module

---
+++ display.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 16:30:58 2017
+
+@author: Richard
+Blank stimulus artifacts and plot the trace
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def blank(trace, facts):
+
+    for i in range(0, len(facts)):
+        blanked = trace
+        p1 = min(facts[i]) - 1
+        p2 = max(facts[i]) + 1
+        p1V = trace[p1]
+        p2V = trace[p2]
+        points = np.linspace(p1, p2, num=(p2-p1), dtype=int)
+        blank = np.interp(points, [p1, p2], [p1V, p2V])
+        blanked[range(p1, p2)] = blank
+
+    plt.plot(blanked)
+
+    return
